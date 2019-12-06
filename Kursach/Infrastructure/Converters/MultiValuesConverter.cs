22fcc949c91174494827b467625e9eb970eca3ad
vano hacker <v.gritskevich25@gmail.com>
2019-12-06 09:52:08 +0300

    pochti gotovo okno generacii statistiki

---
+++ Kursach/Infrastructure/Converters/MultiValuesConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kursach.Infrastructure.Converters
+{
+    public class MultiValuesConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[] { value };
+        }
+    }
+}
---
+++ Kursach/ViewModel/StatisticsWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Kursach.Infrastructure.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace Kursach.ViewModel
+{
+    public class StatisticsWindowViewModel : ViewModelBase
+    {
+        private BaseCommand command;
+        public ICommand CreateStatisticsCommand
+        {
+            get
+            {
+                if(command == null)
+                {
+                    command = new BaseCommand(CreateStatistics, null);
+                }
+                return command;
+            }
+        }
+
+        public void CreateStatistics(object parameter)
+        {
+            object[] parameters = (object[])parameter;
+            DatePicker fPicker = (DatePicker)parameters[0];
+            DatePicker sPicker = (DatePicker)parameters[1];
+
+
+        }
+    }
+}
