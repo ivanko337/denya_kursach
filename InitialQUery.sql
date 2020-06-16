2944bf926a84230a4178585334ef06e56ca967c2
ivanko337 <v.gritskevich25@gmail.com>
2020-06-16 13:00:37 +0300

    db

--- InitialQUery.sql
+++ InitialQUery.sql
@@ -36,7 +36,7 @@ CREATE TABLE Products(
 	p_name NVARCHAR(100) NOT NULL,
 	p_price DECIMAL(10, 2) NOT NULL,
 	product_type INT NOT NULL,
-	product_image_path NVARCHAR(100) NOT NULL,
+	product_image_path NVARCHAR(MAX) NOT NULL,
 	FOREIGN KEY(product_type) REFERENCES ProductsTypes(id)
 );
 
@@ -77,18 +77,28 @@ INSERT INTO IngredientsTypes(ingredient_type_name) VALUES (N'хлебо-було
 
 INSERT INTO ProductsTypes(product_type_name) VALUES (N'шаурма');
 INSERT INTO ProductsTypes(product_type_name) VALUES (N'бургер');
+INSERT INTO ProductsTypes(product_type_name) VALUES (N'суп');
 
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (1, N'кебаб куриный', 300); -- 1
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (6, N'лаваш', 70); -- 2
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (3, N'огурец', 40); -- 3
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'кетчуп', 20); -- 4
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'майонез', 20); -- 5
+INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'курица', 150); -- 6
+INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'яйцо', 100); -- 7
+INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'чипсы', 20); -- 8
+INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'чеснок', 20); -- 9
+INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (5, N'стейк', 400); -- 10
 
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (1, N'котлета говяжья', 200); -- 6
 INSERT INTO Ingredients(i_group, i_name, i_weight) VALUES (6, N'булка', 50); -- 7
 
 INSERT INTO Products(p_name, p_price, product_type, product_image_path) VALUES(N'Шаурма Стандарт', 6, 1, N'/Kursach;component/Resources/sheurma_standart.jpg');
 INSERT INTO Products(p_name, p_price, product_type, product_image_path) VALUES(N'Гамбургер Стандарт', 6, 2, N'/Kursach;component/Resources/hamburger_standart.jpg');
+INSERT INTO Products(p_name, p_price, product_type, product_image_path) VALUES(N'Куриный суп', 12, 3, N'https://ms1.relax.by/images/5da23058500fe4a6857e31cd0906d449/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/40/23/70/4023700d36949269770cfab919839bf5.jpg');
+INSERT INTO Products(p_name, p_price, product_type, product_image_path) VALUES(N'Грибной суп', 12, 3, N'/Kursach;component/Resources/hamburger_standart.jpg');
+INSERT INTO Products(p_name, p_price, product_type, product_image_path) VALUES(N'Завтрак Стандарт', 6, 2, N'https://ms1.relax.by/images/5da23058500fe4a6857e31cd0906d449/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/4a/b2/77/4ab2774f6af0a6747fa19d7f01409df3.jpg');
+INSERT INTO Products(p_name, p_price, product_type, product_image_path) VALUES(N'Стейк', 6, 2, N'https://ms1.relax.by/images/5da23058500fe4a6857e31cd0906d449/resize/w=1200,h=800,q=80,watermark=true/place_gallery_photo/c3/4d/c7/c34dc776cf377a129b9fc17464aec570.jpg');
 
 INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (1, 1);
 INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (1, 2);
@@ -100,6 +110,14 @@ INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (2, 6);
 INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (2, 7);
 INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (2, 4);
 INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (2, 5);
+
+INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (3, 6);
+INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (3, 7);
+INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (3, 8);
+
+INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (4, 10);
+INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (4, 9);
+INSERT INTO IngridientsProducts(product_id, ingredient_id) VALUES (4, 8);
 
 INSERT INTO Orders(order_date) VALUES (GETDATE());
 INSERT INTO Orders(order_date) VALUES (GETDATE());
